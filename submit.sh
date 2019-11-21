f249106b3496f23a3f4093e243d5779b84d6b5c1
MacroUniverse <270174408@qq.com>
2019-11-20 23:42:42 -0600

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -3,5 +3,5 @@
 for ((i = 101; i <= 102; ++i))
 do
     printf "#!/bin/sh\necho ${i}.sh | ./exec.x > ${i}.out\n" > ${i}.slurm
-    sbatch --time=24:0:0 --mem-per-cpu=1024M --cpus-per-task=16 --ntasks=1 --nodes=1 ./${i}.slurm
+    sbatch --gres=killable:0 --time=72:0:0 --mem-per-cpu=1536M --cpus-per-task=16 --ntasks=1 --nodes=1 ./${i}.slurm
 done
